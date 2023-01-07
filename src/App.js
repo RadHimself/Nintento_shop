245927b2b8b2fe4915017f468021219cd819b943
RadHimself <radek.babij@gmail.com>
2023-01-07 15:45:52 +0100

    directory component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import Directory from './components/directory/directory.component';
 
-function App() {
+const App = () => {
+
+const categories = [
+  {
+    id: 1,
+    title: 'Consoles',
+    imageUrl: 'https://pliki.ppe.pl/storage/084be6ab79ec8d00389a/084be6ab79ec8d00389a.jpg',
+  },
+  {
+    id: 2,
+    title: 'Games',
+    imageUrl: 'https://media-cldnry.s-nbcnews.com/image/upload/newscms/2022_23/3556058/220607-switch-games-vl-2x1.jpg',
+  },
+  {
+    id: 3,
+    title: 'Accesories',
+    imageUrl: 'https://wp-uploads.qualbert.com/2022/02/Controller-Buttons.jpg',
+  },
+  {
+    id: 4,
+    title: 'Literature',
+    imageUrl: 'https://i.ytimg.com/vi/Mpkvf-nvdSU/maxresdefault.jpg',
+  },
+  {
+    id: 5,
+    title: 'Colletibles',
+    imageUrl: 'https://fs-prod-cdn.nintendo-europe.com/media/images/10_share_images/others_3/amiibo_4/H2x1_Amiibo_main_image1600w.jpg',
+  },
+]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Directory categories={categories} />
   );
 }
 
